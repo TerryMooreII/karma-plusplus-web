8ffbde3d888d86e929e64cf675ea46472bb837eb
Terry Moore II <terry.moore.ii@gmail.com>
2017-11-14 18:44:31 -0500

    Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -17,24 +17,9 @@
         <div class="panel-block">
           <table class="table is-fullwidth">
             <tr v-for="user in topReceivers" :key="user.userId">
-              <td><img :src="user.user.user.profile.image_32"></td>
-              <td>{{user.user.user.real_name}}</td>
-              <td>{{user.karma}}</td>
-            </tr>
-          </table>
-        </div>
-      </nav>
-    </div>
-   <div class="column">
-      <nav class="panel">
-        <p class="panel-heading">
-          Highest Karma++ Channels
-        </p>
-        <div class="panel-block">
-          <table class="table is-fullwidth">
-            <tr v-for="channel in topChannels" :key="channel.channelId">
-              <td>#{{channel.channel.channel.name}}</td>
-              <td>{{channel.total}}</td>
+              <td><img class="image is-48_48 is-rounded" :src="user.user.user.profile.image_48"></td>
+              <td class="is-size-5">{{user.user.user.real_name}}</td>
+              <td class="is-size-5 has-text-right">{{user.karma}}</td>
             </tr>
           </table>
         </div>
@@ -43,15 +28,32 @@
     <div class="column">
       <nav class="panel">
         <p class="panel-heading">
+          Top Negative Karma++ Givers
+        </p>
+        <div class="panel-block">
+          <table class="table is-fullwidth">
+            <tr v-for="user in topNegativeGivers" :key="user.userId">
+              <td><img class="image is-48_48 is-rounded" :src="user.user.user.profile.image_48"></td>
+              <td class="is-size-5">{{user.user.user.real_name}}</td>
+              <td class="is-size-5 has-text-right">{{user.positiveKarma}}</td>
+            </tr>
+          </table>
+        </div>
+      </nav>
+    </div>
+   
+    <div class="column">
+      <nav class="panel">
+        <p class="panel-heading">
           Top Postive Karma++ Givers
         </p>
         <div class="panel-block">
           <table class="table is-fullwidth">
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
+            <tr v-for="user in topPostiveGivers" :key="user.userId">
+              <td><img class="image is-48_48 is-rounded" :src="user.user.user.profile.image_48"></td>
+              <td class="is-size-5">{{user.user.user.real_name}}</td>
+              <td class="is-size-5 has-text-right">{{user.positiveKarma}}</td>
+            </tr>
           </table>
         </div>
       </nav>
@@ -61,15 +63,22 @@
     <div class="column">
       <nav class="panel">
         <p class="panel-heading">
-          Most negative Karma++ Givers
+          Highest Karma++ Channels
         </p>
         <div class="panel-block">
           <table class="table is-fullwidth">
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
-            <tr><td>pic</td><td>Terry Moore</td><td>100</td></tr>
+            <tr>
+              <th>Channel</th>
+              <th class="has-text-centered">Positive</th>
+              <th class="has-text-centered">Negative</th>
+              <th class="has-text-centered">Total</th>
+            </tr>
+            <tr v-for="channel in topChannels" :key="channel.channelId">
+              <td class="is-size-5">#{{channel.channel.channel.name}}</td>
+              <td class="is-size-5 has-text-centered">{{channel.positiveKarma}}</td>
+              <td class="is-size-5 has-text-centered">-{{channel.negativeKarma}}</td>
+              <td class="is-size-5 has-text-centered">{{channel.total}}</td>
+            </tr>
           </table>
         </div>
       </nav>
@@ -88,13 +97,14 @@ export default {
     return {
       topReceivers: [],
       topChannels: [],
+      topPostiveGivers: [],
+      topNegativeGivers: [],
     };
   },
   created() {
     axios.get('http://Terrys-MBP.attlocal.net:3000/api/receivers/T3QDS4TNY')
     .then((response) => {
       this.topReceivers = response.data;
-      console.log(this.topReceivers);
     })
     .catch((e) => {
       this.errors.push(e);
@@ -103,7 +113,21 @@ export default {
     axios.get('http://Terrys-MBP.attlocal.net:3000/api/channels/T3QDS4TNY')
     .then((response) => {
       this.topChannels = response.data;
-      console.log(this.topChannels);
+    })
+    .catch((e) => {
+      this.errors.push(e);
+    });
+
+    axios.get('http://Terrys-MBP.attlocal.net:3000/api/givers/T3QDS4TNY?orderBy=positiveKarma')
+    .then((response) => {
+      this.topPostiveGivers = response.data;
+    })
+    .catch((e) => {
+      this.errors.push(e);
+    });
+    axios.get('http://Terrys-MBP.attlocal.net:3000/api/givers/T3QDS4TNY?orderBy=negativeKarma')
+    .then((response) => {
+      this.topNegativeGivers = response.data;
     })
     .catch((e) => {
       this.errors.push(e);
@@ -113,5 +137,9 @@ export default {
 </script>
 
 <style scoped>
+
+.is-rounded {
+  border-radius: 50%;
+}
 
 </style>
